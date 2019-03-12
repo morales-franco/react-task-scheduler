b77d39da681474513769a84755f7491c25e4137c
Morales Franco <franco.e.morales@gmail.com>
2019-03-11 22:55:42 -0300

    progress

---
+++ my-task-scheduler/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className = "App" >
+                <nav>
+                    Menu 
+                </nav>       
+                <img src = { logo } className = "App-logo" alt = "logo" / >
+            </div>
+        );
+    }
+}
+
+export default App;
