adc52d2161b626e7a46a6938e45e54b887d9e42b
Morales Franco <franco.e.morales@gmail.com>
2019-03-13 23:34:07 -0300

    fix foms

--- my-task-scheduler/src/components/TaskForm.js
+++ my-task-scheduler/src/components/TaskForm.js
@@ -18,7 +18,10 @@ class TaskForm extends Component{
          */
         this.onChangeValue = this.onChangeValue.bind(this);
         this.onSave = this.onSave.bind(this);
+        this.resetState = this.resetState.bind(this);
     }
+
+    
 
     onChangeValue(e){
         // console.log(e.target.value, e.target.name);
@@ -47,12 +50,24 @@ class TaskForm extends Component{
 
         this.props.onAddTask(this.state);
 
-        this.setState = {
+        this.resetState();
+        this.resetForm();
+    }
+
+    resetState(){
+        this.setState({
             title: "",
             responsible: "",
             description: "",
             priority: "low"
-        }
+        });
+    }
+
+    resetForm(){
+        document.getElementById('title-id').value = "";
+        document.getElementById('responsible-id').value = "";
+        document.getElementById('description-id').value = "";
+        document.getElementById('priority-id').value = "low";
     }
 
     render() {
@@ -61,19 +76,19 @@ class TaskForm extends Component{
                 <div className="card-body">
                     <form onSubmit={this.onSave}>
                         <div className="form-group">
-                            <input type="text" name="title" className="form-control" placeholder="Title" onChange={this.onChangeValue}/>
+                            <input type="text" id="title-id" name="title" className="form-control" placeholder="Title" onChange={this.onChangeValue}/>
                         </div>
                         <div className="form-group">
-                            <input type="text" name="responsible" className="form-control" placeholder="Responsible" onChange={this.onChangeValue}/>
+                            <input type="text" id="responsible-id" name="responsible" className="form-control" placeholder="Responsible" onChange={this.onChangeValue}/>
                         </div>
                         <div className="form-group">
-                            <textarea  name="description" className="form-control" placeholder="Description" rows="3" onChange={this.onChangeValue}/>
+                            <textarea  id="description-id" name="description" className="form-control" placeholder="Description" rows="3" onChange={this.onChangeValue}/>
                         </div>
                         <div className="form-group">
-                            <select className="form-control" name="priority" onChange={this.onChangeValue}>
-                                <option>Low</option>
-                                <option>Medium</option>
-                                <option>High</option>
+                            <select className="form-control" id="priority-id" name="priority" onChange={this.onChangeValue}>
+                                <option value="low">Low</option>
+                                <option value="medium">Medium</option>
+                                <option value="high">High</option>
                             </select>
                         </div>
                         <button className="btn btn-primary">Save</button>
