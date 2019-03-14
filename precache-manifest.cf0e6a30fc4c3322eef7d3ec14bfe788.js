f891cd9c98e8fb55cace893518d1d2545baaf31c
Morales Franco <franco.e.morales@gmail.com>
2019-03-13 23:04:39 -0300

    Updates

---
+++ precache-manifest.cf0e6a30fc4c3322eef7d3ec14bfe788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-task-scheduler/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e8ba5205d3b0de482fb9",
+    "url": "/react-task-scheduler/static/js/runtime~main.1aeabffb.js"
+  },
+  {
+    "revision": "b580e6b37bfb95986ab1",
+    "url": "/react-task-scheduler/static/js/main.ca9177d3.chunk.js"
+  },
+  {
+    "revision": "e975d3f1244271240c56",
+    "url": "/react-task-scheduler/static/js/2.55abee9f.chunk.js"
+  },
+  {
+    "revision": "b580e6b37bfb95986ab1",
+    "url": "/react-task-scheduler/static/css/main.b767d0a5.chunk.css"
+  },
+  {
+    "revision": "d2b3119a39ebeec85582e7db0da22bb6",
+    "url": "/react-task-scheduler/index.html"
+  }
+];
